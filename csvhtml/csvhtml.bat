49e2168ab29f14ce65a7b5a4468fcdb64294fe3f
switchworks <s1224g@gmail.com>
2018-01-17 23:54:24 +0900

    Add files via upload

---
+++ csvhtml/csvhtml.bat
@@ -0,0 +1,11 @@
+@powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
+
+if ($Args[0] -ne $null){
+	$html = gc 'temp.html'
+	gc $Args[0] -Encoding UTF8 | ConvertFrom-CSV | %{
+		$id = $_.id
+		$value = $_.value
+		$html = $html -replace "(id='$id'.*?>)",('$1'+"$value")
+	}
+	$html | Out-File -FilePath ((Get-Item $Args[0]).BaseName + ".html") -Encoding UTF8
+}
